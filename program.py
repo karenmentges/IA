0e8e26d1bf31fe62fad014b9b5162fb509a5452e
Karen Ruver Mentges <karen_ruvermentges@hotmail.com>
2023-11-22 19:29:09 -0300

    Upload program

--- program.py
+++ program.py
@@ -20,13 +20,14 @@ def obter_quantidade_cores(imagem):
 
 def extrair_propriedades(caminho_imagem, imagem):
     altura, largura, canais = imagem.shape                                              # Obter resolução e canais da imagem
-    tamanho_do_arquivo = os.path.getsize(caminho_imagem)                                # Obter tamanho da imagem
+    tamanho_do_arquivo_em_bytes = os.path.getsize(caminho_imagem)                       # Obter tamanho da imagem
+    tamanho_do_arquivo_em_kb = tamanho_do_arquivo_em_bytes/1024 
     media_cores = np.mean(imagem, axis=(0, 1))                                          # Cálcular média das cores
     quantidade_cores = obter_quantidade_cores(imagem)                                   # Obtem quantidade de cores da imagem
     return {
         "altura": altura,
         "largura": largura,
-        "tamanho_do_arquivo (bytes)": tamanho_do_arquivo,
+        "tamanho_do_arquivo (KB)": tamanho_do_arquivo_em_kb,
         "quantidade_cores": quantidade_cores,
         "canais": canais,
         "media_cores": media_cores
@@ -34,7 +35,7 @@ def extrair_propriedades(caminho_imagem, imagem):
 
 def aplicar_k_means(imagem, k):
     pixels = imagem.reshape((-1, 3))                                                    # Redimensionar a imagem para um vetor de pixels
-    kmeans = KMeans(n_clusters=k)                                                       # Aplicar o algoritmo k-médias
+    kmeans = KMeans(n_clusters=k, n_init=10)                                            # Aplicar o algoritmo k-médias
     kmeans.fit(pixels)                                                                  # Encontrar os centros dos clusters
     rótulos = kmeans.labels_                                                            # Obter rótulos dos clusters
     centróides = kmeans.cluster_centers_                                                # Obter centróides dos clusters
@@ -45,7 +46,7 @@ def plotar_imagens(imagem_segmentada):
     plt.imshow(imagem_segmentada.astype(np.uint8))                                      # Mostrar a imagem usando Matplotlib
     plt.axis('off')                                                                     # Desligar os eixos
     plt.tight_layout()                                                                  # Ajustar o layout para preencher a área disponível
-    plt.savefig('image1_3Cores.png', dpi=500, bbox_inches='tight', pad_inches=0.0)      # Exportar a imagem
+    plt.savefig('image1_3Colors.png', dpi=500, bbox_inches='tight', pad_inches=0.0)     # Exportar a imagem
     """ plt.show()   """                                                                # Exibir a imagem
 
 def main():
@@ -61,7 +62,7 @@ def main():
 
     plotar_imagens(imagem_segmentada)                                                               # Exportar e visualizar a imagem segmentada
     
-    caminho_imagem_segmentada = "image1_3Cores.png"                                                 # Definir caminho da imagem segmentada
+    caminho_imagem_segmentada = "image1_3Colors.png"                                                # Definir caminho da imagem segmentada
     propriedades_segmentadas = extrair_propriedades(caminho_imagem_segmentada, imagem_segmentada)   # Extrair as propriedades da imagem segmentada e imprimir
     print(f"\nPropriedades da Imagem Segmentada (k={k}):")
     print(propriedades_segmentadas)
